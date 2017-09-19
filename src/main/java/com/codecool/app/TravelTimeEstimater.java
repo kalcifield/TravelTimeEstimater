1c4090ef864ce270671a7a7ff135cc60d023617f
kalcifield <bezilaszlo95@gmail.com>
2017-09-19 14:24:07 +0200

    initializing the project

---
+++ src/main/java/com/codecool/app/TravelTimeEstimater.java
@@ -0,0 +1,12 @@
+package com.codecool.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTimeEstimater {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelTimeEstimater.class, args);
+    }
+}
